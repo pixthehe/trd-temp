6e2741ea8fc66136ae603345ed22ba0a8267b296
Devin L <65364480+pixthehe@users.noreply.github.com>
2023-05-20 12:34:42 -0400

    Update trd-script.lua

--- trd-script.lua
+++ trd-script.lua
@@ -15,11 +15,25 @@ local TextButton7 = Instance.new("TextButton")
 local TextButton8 = Instance.new("TextButton")
 local delete = Instance.new("TextButton")
 local minus = Instance.new("TextButton")
+local meth = Instance.new("TextButton")
 local Frame = Instance.new("Frame")
 local deleteno = Instance.new("TextButton")
 local deleteconfirm = Instance.new("TextButton")
 local minusf = Instance.new("Frame")
 local plus = Instance.new("TextButton")
+local methf = Instance.new("Frame")
+local UIGridLayout = Instance.new("UIGridLayout")
+local _1 = Instance.new("TextLabel")
+local _2 = Instance.new("TextLabel")
+local _3 = Instance.new("TextLabel")
+local _4 = Instance.new("TextLabel")
+local _5 = Instance.new("TextLabel")
+local _6 = Instance.new("TextLabel")
+local _7 = Instance.new("TextLabel")
+local _8 = Instance.new("TextLabel")
+local _9 = Instance.new("TextLabel")
+local _10 = Instance.new("TextLabel")
+local gobackmath = Instance.new("TextButton")
 
 --Properties:
 
@@ -38,8 +52,8 @@ MainFrame.ImageColor3 = Color3.fromRGB(154, 255, 116)
 MainFrame.ScaleType = Enum.ScaleType.Slice
 MainFrame.SliceCenter = Rect.new(100, 100, 100, 100)
 MainFrame.SliceScale = 0.250
-MainFrame.Active = true
-MainFrame.Draggable = true
+MainFrame.Active = true;
+MainFrame.Draggable = true;
 
 TextButton1.Name = "TextButton1"
 TextButton1.Parent = MainFrame
@@ -87,7 +101,7 @@ TextButton5.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextButton5.Position = UDim2.new(0.0425761938, 0, 0.548865199, 0)
 TextButton5.Size = UDim2.new(0, 134, 0, 43)
 TextButton5.Font = Enum.Font.SourceSans
-TextButton5.Text = "walk when ur olivia borman"
+TextButton5.Text = "walk when movement blocked"
 TextButton5.TextColor3 = Color3.fromRGB(0, 0, 0)
 TextButton5.TextSize = 14.000
 TextButton5.TextWrapped = true
@@ -98,7 +112,7 @@ TextButton6.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextButton6.Position = UDim2.new(0.519011319, 0, 0.545255125, 0)
 TextButton6.Size = UDim2.new(0, 134, 0, 43)
 TextButton6.Font = Enum.Font.SourceSans
-TextButton6.Text = "infyeild(i didnt make this one but yuh)"
+TextButton6.Text = "inf yeild"
 TextButton6.TextColor3 = Color3.fromRGB(0, 0, 0)
 TextButton6.TextSize = 14.000
 
@@ -142,6 +156,18 @@ minus.Font = Enum.Font.SourceSans
 minus.Text = "-"
 minus.TextColor3 = Color3.fromRGB(0, 0, 0)
 minus.TextSize = 91.000
+minusf.Active = true;
+minusf.Draggable = true;
+minusf.Visible = false;
+
+meth.Name = "meth"
+meth.Parent = MainFrame
+meth.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+meth.Position = UDim2.new(0.873494148, 0, 0.960288763, 0)
+meth.Size = UDim2.new(0, 24, 0, 9)
+meth.Font = Enum.Font.SourceSans
+meth.TextColor3 = Color3.fromRGB(0, 0, 0)
+meth.TextSize = 14.000
 
 Frame.Parent = anal4life
 Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
@@ -174,9 +200,6 @@ minusf.Parent = anal4life
 minusf.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 minusf.Position = UDim2.new(0.361764729, 0, 0.122853376, 0)
 minusf.Size = UDim2.new(0, 241, 0, 40)
-minusf.Visible = false;
-minusf.Active = true;
-minusf.Draggable = true;
 
 plus.Name = "plus"
 plus.Parent = minusf
@@ -188,127 +211,278 @@ plus.Text = "+"
 plus.TextColor3 = Color3.fromRGB(0, 0, 0)
 plus.TextSize = 100.000
 
+methf.Name = "methf"
+methf.Parent = anal4life
+methf.BackgroundColor3 = Color3.fromRGB(255, 80, 106)
+methf.Position = UDim2.new(0.00147058815, 0, 0.670138896, 0)
+methf.Size = UDim2.new(0, 406, 0, 285)
+methf.Visible = false;
+
+UIGridLayout.Parent = methf
+UIGridLayout.SortOrder = Enum.SortOrder.LayoutOrder
+UIGridLayout.CellSize = UDim2.new(0, 100, 0, 60)
+
+_1.Name = "1"
+_1.Parent = methf
+_1.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_1.Size = UDim2.new(0, 71, 0, 75)
+_1.Font = Enum.Font.SourceSans
+_1.TextColor3 = Color3.fromRGB(0, 0, 0)
+_1.TextSize = 14.000
+
+_2.Name = "2"
+_2.Parent = methf
+_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_2.Size = UDim2.new(0, 71, 0, 75)
+_2.Font = Enum.Font.SourceSans
+_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+_2.TextSize = 14.000
+
+_3.Name = "3"
+_3.Parent = methf
+_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_3.Size = UDim2.new(0, 71, 0, 75)
+_3.Font = Enum.Font.SourceSans
+_3.TextColor3 = Color3.fromRGB(0, 0, 0)
+_3.TextSize = 14.000
+
+_4.Name = "4"
+_4.Parent = methf
+_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_4.Size = UDim2.new(0, 71, 0, 75)
+_4.Font = Enum.Font.SourceSans
+_4.TextColor3 = Color3.fromRGB(0, 0, 0)
+_4.TextSize = 14.000
+
+_5.Name = "5"
+_5.Parent = methf
+_5.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_5.Size = UDim2.new(0, 71, 0, 75)
+_5.Font = Enum.Font.SourceSans
+_5.TextColor3 = Color3.fromRGB(0, 0, 0)
+_5.TextSize = 14.000
+
+_6.Name = "6"
+_6.Parent = methf
+_6.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_6.Size = UDim2.new(0, 71, 0, 75)
+_6.Font = Enum.Font.SourceSans
+_6.TextColor3 = Color3.fromRGB(0, 0, 0)
+_6.TextSize = 14.000
+
+_7.Name = "7"
+_7.Parent = methf
+_7.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_7.Size = UDim2.new(0, 71, 0, 75)
+_7.Font = Enum.Font.SourceSans
+_7.TextColor3 = Color3.fromRGB(0, 0, 0)
+_7.TextSize = 14.000
+
+_8.Name = "8"
+_8.Parent = methf
+_8.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_8.Size = UDim2.new(0, 71, 0, 75)
+_8.Font = Enum.Font.SourceSans
+_8.TextColor3 = Color3.fromRGB(0, 0, 0)
+_8.TextSize = 14.000
+
+_9.Name = "9"
+_9.Parent = methf
+_9.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_9.Size = UDim2.new(0, 71, 0, 75)
+_9.Font = Enum.Font.SourceSans
+_9.TextColor3 = Color3.fromRGB(0, 0, 0)
+_9.TextSize = 14.000
+
+_10.Name = "10"
+_10.Parent = methf
+_10.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_10.Size = UDim2.new(0, 71, 0, 75)
+_10.Font = Enum.Font.SourceSans
+_10.TextColor3 = Color3.fromRGB(0, 0, 0)
+_10.TextSize = 14.000
+
+gobackmath.Name = "gobackmath"
+gobackmath.Parent = methf
+gobackmath.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+gobackmath.Size = UDim2.new(0, 200, 0, 50)
+gobackmath.Font = Enum.Font.SourceSans
+gobackmath.Text = "BackButton"
+gobackmath.TextColor3 = Color3.fromRGB(0, 0, 0)
+gobackmath.TextSize = 14.000
+
+
+-- UI STUFF
 delete.MouseButton1Down:Connect(function()
-    Frame.Visible = true;
+	Frame.Visible = true;
 end)
 
 deleteno.MouseButton1Down:Connect(function()
-    Frame.Visible = false;
+	Frame.Visible = false;
 end)
 
 deleteconfirm.MouseButton1Down:Connect(function()
-    anal4life:Destroy()
+	anal4life:Destroy()
 end)
 
 minus.MouseButton1Down:Connect(function()
-    MainFrame.Visible = false;
-    minusf.Visible = true;
+	MainFrame.Visible = false;
+	minusf.Visible = true;
 end)
 
 plus.MouseButton1Down:Connect(function()
-    MainFrame.Visible = true;
-    minusf.Visible = false;
+	MainFrame.Visible = true;
+	minusf.Visible = false;
 end)
 
 
- -- TEXT BOXES
+
+--MATH
+
+meth.MouseButton1Down:Connect(function()
+	methf.Visible=true;
+	for i=1,11 do
+		for i,v in pairs(game.workspace.Assets["Math Mania"]["MathMania"]:GetChildren()) do
+			print(v)
+			if v.Name == "1" then 
+				print(tostring(v.Answer.Value))
+				_1.Text = "1: "..v.Answer.Value
+			elseif v.Name == "2" then 
+				print(tostring(v.Answer.Value))
+				_2.Text = "2: "..v.Answer.Value
+			elseif v.Name == "3" then 
+				print(tostring(v.Answer.Value))
+				_3.Text = "3: "..v.Answer.Value
+			elseif v.Name == "4" then 
+				print(tostring(v.Answer.Value))
+				_4.Text = "4: "..v.Answer.Value
+			elseif v.Name == "5" then 
+				print(tostring(v.Answer.Value))
+				_5.Text = "5: "..v.Answer.Value
+			elseif v.Name == "6" then 
+				print(tostring(v.Answer.Value))
+				_6.Text = "6: "..v.Answer.Value
+			elseif v.Name == "7" then 
+				print(tostring(v.Answer.Value))
+				_7.Text = "7: "..v.Answer.Value
+			elseif v.Name == "8" then 
+				print(tostring(v.Answer.Value))
+				_8.Text = "8: "..v.Answer.Value
+			elseif v.Name == "9" then 
+				print(tostring(v.Answer.Value))
+				_9.Text = "9: "..v.Answer.Value
+			elseif v.Name == "10" then 
+				print(tostring(v.Answer.Value))
+				_10.Text = "10: "..v.Answer.Value
+			end
+		end
+	end
+end)
+
+gobackmath.MouseButton1Down:Connect(function()
+	methf.Visible = false;
+end)
+
+
+
+-- VUTTONS
 TextButton1.MouseButton1Down:Connect(function()
-    plr = game.Players.LocalPlayer
-    hum = plr.Character.HumanoidRootPart
+	plr = game.Players.LocalPlayer
+	hum = plr.Character.HumanoidRootPart
 
-    wait(.1)
-        for i, v in pairs(game.workspace.Idols:GetChildren()) do
-            if i == 4 then 
-                print("Saftey Stacute here")
-            else
-                print("safty statuce no here")
-            end
-        end
+	wait(.1)
+	for i, v in pairs(game.workspace.Idols:GetChildren()) do
+		if i == 4 then 
+			print("Saftey Stacute here")
+		else
+			print("safty statuce no here")
+		end
+	end
 
-        for i, v in pairs(game.workspace.Idols:GetChildren()) do
-            wait(1)
-            if i == 1 and v.Name == "Bag" then
-                hum.CFrame = v.MeshPart.CFrame
-            end
-        end
+	for i, v in pairs(game.workspace.Idols:GetChildren()) do
+		wait(1)
+		if i == 1 and v.Name == "Bag" then
+			hum.CFrame = v.MeshPart.CFrame
+		end
+	end
 
-    wait(2) -- wait delay to fix somehgtung idk
+	wait(2) -- wait delay to fix somehgtung idk
 
-        for i, v in pairs(game.workspace.Idols:GetChildren()) do
-            if v.Name == "SafetyStatue" then
-                hum.CFrame = v.Handle2.CFrame
-            end
-        end
+	for i, v in pairs(game.workspace.Idols:GetChildren()) do
+		if v.Name == "SafetyStatue" then
+			hum.CFrame = v.Handle2.CFrame
+		end
+	end
 end)
 
 TextButton2.MouseButton1Down:Connect(function()
-    plr = game.Players.LocalPlayer
-    hum = plr.Character.HumanoidRootPart
-    
-    for i, v in pairs(game.workspace.Assets:GetChildren()) do
-        for _, v2 in pairs(v:GetChildren()) do
-            print(v)
-            for _, v2 in pairs(v:GetChildren()) do
-                print(v2)
-                if v2.Name == "Finish" then
-                    hum.CFrame = v2.CFrame
-                end
-            end
-        end
-    end
+	plr = game.Players.LocalPlayer
+	hum = plr.Character.HumanoidRootPart
+
+	for i, v in pairs(game.workspace.Assets:GetChildren()) do
+		for _, v2 in pairs(v:GetChildren()) do
+			print(v)
+			for _, v2 in pairs(v:GetChildren()) do
+				print(v2)
+				if v2.Name == "Finish" then
+					hum.CFrame = v2.CFrame
+				end
+			end
+		end
+	end
 end)
 
 TextButton3.MouseButton1Down:Connect(function()
-    game.workspace.Glass:Destroy()
+	game.workspace.Glass:Destroy()
 end)
 
 TextButton4.MouseButton1Down:Connect(function()
-    for i, v in pairs(game.workspace.Assets:GetChildren()) do
-        for _, v2 in pairs(v:GetChildren()) do
-            print(v)
-            for _, v2 in pairs(v:GetChildren()) do
-                print(v2)
-                if v2.Name == "Part" and v2:FindFirstChild("Weld") then
-                    v2:Destroy()
-                end
-            end
-        end
-    end
+	for i, v in pairs(game.workspace.Assets:GetChildren()) do
+		for _, v2 in pairs(v:GetChildren()) do
+			print(v)
+			for _, v2 in pairs(v:GetChildren()) do
+				print(v2)
+				if v2.Name == "Part" and v2:FindFirstChild("Weld") then
+					v2:Destroy()
+				end
+			end
+		end
+	end
 end)
 
 TextButton5.MouseButton1Down:Connect(function()
-    getgenv().WalkSpeedValue = 16;
-    local Player = game:service'Players'.LocalPlayer;
-    Player.Character.Humanoid:GetPropertyChangedSignal'WalkSpeed':Connect(function()
-    Player.Character.Humanoid.WalkSpeed = getgenv().WalkSpeedValue;
-    end)
-    Player.Character.Humanoid.WalkSpeed = getgenv().WalkSpeedValue; 
-    getgenv().WalkSpeedValue = 17
-    wait(.1)
-    getgenv().WalkSpeedValue = 16
+	getgenv().WalkSpeedValue = 16;
+	local Player = game:service'Players'.LocalPlayer;
+	Player.Character.Humanoid:GetPropertyChangedSignal'WalkSpeed':Connect(function()
+		Player.Character.Humanoid.WalkSpeed = getgenv().WalkSpeedValue;
+	end)
+	Player.Character.Humanoid.WalkSpeed = getgenv().WalkSpeedValue; 
+	getgenv().WalkSpeedValue = 17
+	wait(.1)
+	getgenv().WalkSpeedValue = 16
 end)
 
 TextButton6.MouseButton1Down:Connect(function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 end)
 
 
 TextButton8.MouseButton1Down:Connect(function()
-    getgenv().coins = true
-    while true do
-        wait(.1)
-        if not getgenv().coins then return end
-        print("on")
-        for i, v in pairs(game.workspace.Assets["Coin Hunt"].Coins) do
-            if v.Name == "Coin" or v.Name == "Gem" then
-                hum.CFrame = v.CFrame
-                wait(.3)
-            end
-        end
-    end
+	getgenv().coins = true
+	while true do
+		wait(.1)
+		if not getgenv().coins then return end
+		print("on")
+		for i, v in pairs(game.workspace.Assets["Coin Hunt"].Coins) do
+			if v.Name == "Coin" or v.Name == "Gem" then
+				hum.CFrame = v.CFrame
+				wait(.3)
+			end
+		end
+	end
 end)
 
 TextButton7.MouseButton1Down:Connect(function()
-    getgenv().coins = false
-    print("off")
+	getgenv().coins = false
+	print("off")
 end)
